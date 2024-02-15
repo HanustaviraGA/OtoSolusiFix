bc25917da286f793f70bad408d671d839b431bcf
HanustaviraGA <hanustavira.acarya@binus.ac.id>
2024-02-15 17:41:33 +0700

    SQL file

---
+++ otomotif_solusi.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100428 (10.4.28-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : otomotif_solusi
+
+ Target Server Type    : MySQL
+ Target Server Version : 100428 (10.4.28-MariaDB)
+ File Encoding         : 65001
+
+ Date: 15/02/2024 17:40:10
+*/
+
